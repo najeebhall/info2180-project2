aeb9cde3c89f73cfb67dc74b1368dcc69e5e26d0
najeeb <naj404@hotmail.com>
2017-10-31 00:57:19 -0500

    my second commit

--- fifteen.js
+++ fifteen.js
@@ -1,3 +1,20 @@
-window.onload function() {
+window.onload = function() {
+	var walls = document.getElementById("puzzlearea");
+	var tiles = walls.getElementsByTagName('div');
+	for (var i = 0; i < tiles.length; i++) {
+		tiles[i].setAttribute("class", "puzzlepiece");
+		//tiles[i].className = 'puzzlepiece';
+    	tiles[i].style.left = (i%4*100)+'px';
+    	tiles[i].style.top = (parseInt(i/4)*100) + 'px';
+    	tiles[i].style.backgroundPosition= '-' + tiles[i].style.left + ' ' + '-' + tiles[i].style.top;
 	}
+	
+	//var wall = document.getElementById("puzzlearea");
+	//walls.addclass('puzzlepiece');
+	/*var puzzlearea = $(#puzzlearea);
+	var tiles = puzzlearea.children();
+	for (var i = 0, i < tiles.length, i++) {
+		tiles=.addclass(puzzlepiece);
+
+	}*/
 }
