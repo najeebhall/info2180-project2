751faa0a1d3099d673ab2ac73b70fb9becf2ffdb
najeeb <naj404@hotmail.com>
2017-11-04 20:54:41 -0500

    my fourth commit

--- fifteen.js
+++ fifteen.js
@@ -1,3 +1,6 @@
+var posX;
+var posY;
+var pieces;
 window.onload = function() {
 	var walls = document.getElementById("puzzlearea");
 	var tiles = walls.getElementsByTagName('div');
@@ -38,5 +41,85 @@ window.onload = function() {
         }
       }
     }
+  }
+}
+
+function moveLeft(x, y)
+{
+  var xx = parseInt(x);
+  var yy = parseInt(y);
+
+  if (xx > 0)
+  {
+    for (var m = 0; m < pieces.length; m++)
+    {
+      if (parseInt(pieces[m].style.left) + 100 == xx && parseInt(pieces[m].style.top) == yy)
+      {
+        return m;
+      }
+    }
+  }
+  else
+  {
+    return -1;
+  }
+}
+
+function moveRight(x, y)
+{
+  var xx = parseInt(x);
+  var yy = parseInt(y);
+  if (xx < 300)
+  {
+    for (var n =0; n < pieces.length; n++)
+    {
+      if (parseInt(pieces[n].style.left) - 100 == xx && parseInt(pieces[n].style.top) == yy)
+      {
+        return n;
+      }
+    }
+  }
+  else
+  {
+    return -1;
+  }
+}
+
+function moveUp(x, y)
+{
+  var xx = parseInt(x);
+  var yy = parseInt(y);
+  if (yy > 0)
+  {
+    for (var o = 0; o < pieces.length; o++){
+      if (parseInt(pieces[o].style.top) + 100 == yy && parseInt(pieces[o].style.left) == xx)
+      {
+        return o;
+      }
+    }
+  }
+  else
+  {
+    return -1;
+  }
+}
+
+function moveDown(x, y)
+{
+  var xx = parseInt(x);
+  var yy = parseInt(y);
+  if (yy < 300)
+  {
+    for (var p = 0; p < pieces.length; p++)
+    {
+      if (parseInt(pieces[p].style.top) - 100 == yy && parseInt(pieces[p].style.left) == xx)
+      {
+        return p;
+      }
+    }
+  }
+  else
+  {
+    return -1;
   }
 }
