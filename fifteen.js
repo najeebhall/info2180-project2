8dfb0538e786135f7010a5fcbdc70ccc7918bd8c
najeeb <naj404@hotmail.com>
2017-11-04 20:53:00 -0500

    my third commit

--- fifteen.js
+++ fifteen.js
@@ -8,13 +8,35 @@ window.onload = function() {
     	tiles[i].style.top = (parseInt(i/4)*100) + 'px';
     	tiles[i].style.backgroundPosition= '-' + tiles[i].style.left + ' ' + '-' + tiles[i].style.top;
 	}
-	
-	//var wall = document.getElementById("puzzlearea");
-	//walls.addclass('puzzlepiece');
-	/*var puzzlearea = $(#puzzlearea);
-	var tiles = puzzlearea.children();
-	for (var i = 0, i < tiles.length, i++) {
-		tiles=.addclass(puzzlepiece);
-
-	}*/
+	var shufflebutton = document.getElementById('shufflebutton');
+  shufflebutton.onclick = function(){
+  	for (var k = 0; k < 250; k++){
+      var l = parseInt(Math.random()* 100) %4;
+      if (l == 0){
+        var temp = moveUp(posX, posY);
+        if (temp != -1){
+          swap(temp);
+        }
+      }
+      if (l == 1){
+        var temp = moveDown(posX, posY);
+        if (temp != -1){
+          swap(temp);
+        }
+      }
+      if (l == 2){
+        var temp = moveLeft(posX, posY);
+        if (temp != -1){
+          swap(temp);
+        }
+      }
+      if (l == 3)
+      {
+        var temp = moveRight(posX, posY);
+        if (temp != -1){
+          swap(temp);
+        }
+      }
+    }
+  }
 }
